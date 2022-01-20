eb0d9796889b8a2302478a9fbfc7f15fd0c6d65e
ejarans1 <32781829+ejarans1@users.noreply.github.com>
2022-01-20 12:04:18 -0500

    New Skybox and Furniture

--- My project/Assets/Scripts/PlayerBehavior/PlayerController.cs
+++ My project/Assets/Scripts/PlayerBehavior/PlayerController.cs
@@ -15,10 +15,10 @@ public class PlayerController : MonoBehaviour {
     space : Moves camera on X and Z axis only.  So camera doesn't gain any height*/
      
      
-    float mainSpeed = 12.5f; //regular speed
-    float shiftAdd = 250.0f; //multiplied by how long shift is held.  Basically running
-    float maxShift = 1000.0f; //Maximum speed when holdin gshift
-    float camSens = 0.25f; //How sensitive it with mouse
+    public float mainSpeed = 12.5f; //regular speed
+    public float shiftAdd = 250.0f; //multiplied by how long shift is held.  Basically running
+    public float maxShift = 1000.0f; //Maximum speed when holdin gshift
+    public float camSens = 0.25f; //How sensitive it with mouse
     private Vector3 lastMouse = new Vector3(255, 255, 255); //kind of in the middle of the screen, rather than at the top (play)
     private float totalRun= 1.0f;
     public Animator animator;
