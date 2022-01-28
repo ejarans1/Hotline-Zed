dad4423f6229cd11286a557b6d5a141e9005e4ba
ejarans1 <32781829+ejarans1@users.noreply.github.com>
2022-01-28 09:41:18 -0500

    Fully Working Generation
    Next Step is to create tracker service that connects with procedural service which will dictacte where tiles can spawn

--- My project/Assets/Scripts/Services/ProceduralEnvironmentGenerationService.cs
+++ My project/Assets/Scripts/Services/ProceduralEnvironmentGenerationService.cs
@@ -30,7 +30,8 @@ public class ProceduralEnvironmentGenerationService : MonoBehaviour
     void Update()
     {
         if (generateNewPlatFormFlag){
-            generateNewPlatform();
+            Vector3 spawnPosition = getSpawnPositions(currentTilePosition); 
+            generateNewPlatform(spawnPosition);
             moveGeneratedTile(generatedPrefab);
             linkGeneratedPrefabToProgressionController();
             updateProceduralServiceWithNewPlatformValues();
@@ -42,10 +43,10 @@ public class ProceduralEnvironmentGenerationService : MonoBehaviour
         currentTilePosition = generatedPrefab.transform;
         //currentTilePosition.transform.position = generatePositionWithOffset(generatedPrefab.transform, 0, 0, 0);
     }
-    private void generateNewPlatform(){
+    private void generateNewPlatform(Vector3 spawnPosition){
         generateNewPlatFormFlag = false; 
         prefabToUse = proceduralPrefab;
-        generatedPrefab = generatePreFabAtSpawnPosition(prefabToUse, currentTilePosition);
+        generatedPrefab = generatePreFabAtSpawnPosition(prefabToUse, spawnPosition, currentTilePosition);
         
     }
 
@@ -54,13 +55,13 @@ public class ProceduralEnvironmentGenerationService : MonoBehaviour
     }
 
     private void moveGeneratedTile(GameObject prefabToMove){
-        EnvironmentTileMover environmentTileMoverOfGeneratedPrefab = generatedPrefab.GetComponent<EnvironmentTileMover>();
-        environmentTileMoverOfGeneratedPrefab.setPositionToMoveTowards(generatePositionWithOffset(currentTilePosition, 40, 0, 0));
+        Vector3 newPosition = generatePositionWithOffset(generatedPrefab.transform, 0, 20, 0); 
+        EnvironmentTileMover environmentTileMoverOfGeneratedPrefab = generatedPrefab.GetComponent<EnvironmentTileMover>();;
+        environmentTileMoverOfGeneratedPrefab.setPositionToMoveTowards(newPosition);
         environmentTileMoverOfGeneratedPrefab.triggerTileMovement();
     }
-    private GameObject generatePreFabAtSpawnPosition(GameObject prefabToUse, Transform  spawnPosition){
-        Vector3 offsetPosition = generatePositionWithOffset(spawnPosition, 40, -20, 0);
-        GameObject gameObjectPrefab = Instantiate(prefabToUse, offsetPosition, spawnPosition.rotation, environmentParent);
+    private GameObject generatePreFabAtSpawnPosition(GameObject prefabToUse, Vector3  spawnPosition, Transform tileRotation){
+        GameObject gameObjectPrefab = Instantiate(prefabToUse, spawnPosition, tileRotation.rotation, environmentParent);
         return gameObjectPrefab;
     }
 
@@ -69,6 +70,16 @@ public class ProceduralEnvironmentGenerationService : MonoBehaviour
         float y = originalPosition.transform.position.y + yOffset;
         float z = originalPosition.transform.position.z + zOffset;
         return new Vector3(x,y,z);
+    }
+    //WorldTileService Creation is neccessary to track tile positioning using matrix array
+    private Vector3 getSpawnPositions(Transform originalPosition){
+        List<Vector3> spawnLocations = new List<Vector3>();
+        spawnLocations.Add(generatePositionWithOffset(originalPosition, 40, -20, 0));
+        spawnLocations.Add(generatePositionWithOffset(originalPosition, -40, -20, 0));
+        spawnLocations.Add(generatePositionWithOffset(originalPosition, 0, -20, 40));
+        spawnLocations.Add(generatePositionWithOffset(originalPosition, 0, -20, 40));
+        
+        return spawnLocations[(Random.Range(0, spawnLocations.Capacity))];
     }
 
     private GameObject getPreFabToUse(){
