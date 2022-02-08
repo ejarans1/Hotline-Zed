99d3184ea7453dac18e80fe3f667ff31be2b0251
ejarans1 <32781829+ejarans1@users.noreply.github.com>
2022-02-07 20:15:45 -0500

    Fixed Indexing Issue for Tile Generation

--- My project/Assets/Scripts/Services/ItemServices/RandomPrefabService.cs
+++ My project/Assets/Scripts/Services/ItemServices/RandomPrefabService.cs
@@ -19,7 +19,7 @@ public class RandomPrefabService : MonoBehaviour
     public GameObject getRandomPrefab(){
         List<GameObject> prefabsToChooseFrom = new List<GameObject>();
         int amountOfPrefab = gameObject.transform.childCount;
-        for (int i = 1; i < amountOfPrefab; i++){
+        for (int i = 0; i < amountOfPrefab; i++){
             prefabsToChooseFrom.Add(gameObject.transform.GetChild(i).gameObject);
         }
         return prefabsToChooseFrom[(Random.Range(0, this.transform.childCount))];
