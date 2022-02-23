8124eaa45b75f5bd52ebb9571ee5f43dfab69221
ejarans1 <32781829+ejarans1@users.noreply.github.com>
2022-02-23 09:28:11 -0500

    HZ-001 - Refactored Inventory Code

--- My project/Assets/Scripts/UI/InventoryUIHandler.cs
+++ My project/Assets/Scripts/UI/InventoryUIHandler.cs
@@ -1,112 +1,41 @@
-using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 using UnityEngine.UI;
+using Quaternion = UnityEngine.Quaternion;
+using Vector3 = UnityEngine.Vector3;
 
 public class InventoryUIHandler : MonoBehaviour
 {
     // Start is called before the first frame update
-    public Image sampleSprite;
-    public RectTransform ParentPanel;
+    public RectTransform parentPanel;
     public float inventoryWidth;
     public float inventoryHeight;
     public float maxInventoryItemsY;
     public float maxInventoryItemsX;
-    private float inventoryTileHeight;
-    private float inventoryTileWidth;
 
     private float xStartingPosition;
     private float yStartingPosition;
 
-    private List<Image> inventoryItems;
     private int inventorySizeXAxis;
-    private int inventorySizeYAxis;
     private int inventoryPopulatedYPosition;
     private int inventoryPopulatedXPosition;
     private List<List<Image>> inventoryMatrix;
 
 
 
-    void Start()
-    {
-        inventoryHeight = 600;
-        inventoryWidth = 700;
-
-        maxInventoryItemsX = 10;
-        maxInventoryItemsY = 10;
-
-        inventoryTileHeight = inventoryHeight / maxInventoryItemsX;
-        inventoryTileWidth = inventoryWidth / maxInventoryItemsY; 
-
-        inventorySizeXAxis = 10;
-        inventorySizeYAxis = 10;
-
-        inventoryPopulatedYPosition = 0;
-        inventoryPopulatedXPosition = 0;
-
-        xStartingPosition = -1;
-        yStartingPosition = 0;
-
-
-
-        inventoryMatrix = new List<List<Image>>();
-
-        for (int columnsOfInventory = 0; columnsOfInventory < inventorySizeXAxis; columnsOfInventory++ ){
-            List<Image> row = new List<Image>();
-            inventoryMatrix.Add(row);
-        }
-
-        List<GameObject> inventoryItems = searchForInventoryItemsInWorldSpace();
-        List<Image> inventoryImages = obtainImageSpritesForInventoryItems(inventoryItems); 
-        //populateImageSpritesIntoInventoryPanel(inventoryImages);
-
+    void Start() {
+        initializeInventoryTrackerValues();
+        populateInventoryColumns();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         toggleInventoryPanel();
-    }
-
-    void FixedUpdate(){
-
-    }
-
-    public Vector3 getTopRightCorner()
-    {
-        Vector3[] v = new Vector3[4];
-        ParentPanel.GetLocalCorners(v);
-        return v[0];
-
-    }
-
-    private List<GameObject> searchForInventoryItemsInWorldSpace (){
-        List <GameObject> inventoryItems = new List<GameObject>();
-        return inventoryItems;
-    }
-
-    private List<Image> obtainImageSpritesForInventoryItems(List<GameObject> inventoryItems){
-        List<Image> inventorySprites = new List<Image>();
-        inventorySprites.Add(sampleSprite);
-        return inventorySprites;
-    }
-
-    private void populateImageSpritesIntoInventoryPanel(List<Image> inventorySprites){
-        int numberOfInventoryItems = inventorySprites.Count;
-        foreach (Image image in inventorySprites){
-            for (int inventoryRowX = 0; inventoryRowX < maxInventoryItemsX; inventoryRowX++){
-                for (int inventoryColumnY = 0; inventoryColumnY < maxInventoryItemsY; inventoryColumnY++){
-                    Debug.Log("AboutToAdd");
-                    addItemToInventoryMatrix(image, inventoryColumnY, inventoryRowX);
-                }
-            }
-           
-        }
     }
 
     public void populateSingleItemToInventory(Image imageSprite){
         checkCurrentInventoryRowAndColumnLimits();
-        addItemToInventoryMatrix(imageSprite,inventoryPopulatedYPosition, inventoryPopulatedXPosition);
+        addItemToInventoryMatrix(imageSprite,inventoryPopulatedYPosition);
         inventoryPopulatedXPosition++;
     }
 
@@ -120,65 +49,64 @@ public class InventoryUIHandler : MonoBehaviour
         }
     }
 
-    private GameObject generatePreFabAtSpawnPosition(){
-        GameObject NewObj = new GameObject(); //Create the GameObject
-        Image NewImage = NewObj.AddComponent<Image>();
-        NewImage.sprite = sampleSprite.sprite;
-        NewObj.GetComponent<RectTransform>().SetParent(ParentPanel.transform);
-        NewObj.SetActive(true); //Activate the GameObject
-        return NewObj;
-    }
-
-    
-    public void addItemToInventoryMatrix(Image newInventoryItem, int yPosition, int xPosition){
+    public void addItemToInventoryMatrix(Image newInventoryItem, int yPosition){
         List<Image> inventoryColumn = inventoryMatrix[yPosition];
         if (inventoryColumn.Count < maxInventoryItemsX){
-            generatePreFabAtSpawnPosition(newInventoryItem, xPosition, yPosition);
+            generatePreFabAtSpawnPosition(newInventoryItem);
             inventoryColumn.Add(newInventoryItem);
         }
-        } 
+    } 
 
-        private GameObject generatePreFabAtSpawnPosition(GameObject prefabToUse, Vector3  spawnPosition, Quaternion swingRotation){
-        GameObject gameObjectPrefab = Instantiate(prefabToUse, spawnPosition, swingRotation);
-        return gameObjectPrefab;
+    private GameObject generatePreFabAtSpawnPosition(Image imageToUse) {
+        GameObject inventoryItem = initializeInventoryItemWithImageAndButton(imageToUse);
+        setPositionOfItemInInventory(inventoryItem);
+        enableInventoryItem(inventoryItem);
+        return inventoryItem;
     }
 
-        private GameObject generatePreFabAtSpawnPosition(Image imageToUse, float xPosition, float yPosition){
-            GameObject NewObj = new GameObject(); //Create the GameObject
-            Button newButton = NewObj.AddComponent<Button>();
-            ColorBlock colorVar = newButton.colors;
-            colorVar.highlightedColor = new Color(91, 192, 41);
-            newButton.colors = colorVar;
-            Image NewImage = NewObj.AddComponent<Image>();
-            newButton.targetGraphic = NewImage;
-            
-            float xOffset = calculateImageXPosition(xPosition);
-            float yOffset = calculateImageYPosition(yPosition);
-            NewImage.sprite = imageToUse.sprite;
-            NewObj.GetComponent<RectTransform>().SetParent(ParentPanel.transform);
-            Vector3 topRight = getTopRightCorner();
-            Debug.Log(topRight.ToString());
-            NewObj.transform.localPosition = generatePositionWizthOffset(topRight, xOffset, yOffset, 10);
-       
-            
-
-        NewObj.SetActive(true); //Activate the GameObject
-            return NewObj;
+    private static GameObject initializeInventoryItemWithImageAndButton(Image imageToUse)
+    {
+        GameObject inventoryItem = new GameObject();
+        Button newButton = configureButtonForInventoryInteraction(inventoryItem);
+        Image newImage = inventoryItem.AddComponent<Image>();
+        newButton.targetGraphic = newImage;
+        newImage.sprite = imageToUse.sprite;
+        return inventoryItem;
     }
 
-       private bool isInventoryRowMaxedOut(int rowToCheck){
-        return false; 
+    private static Button configureButtonForInventoryInteraction(GameObject newObj)
+    {
+        Button newButton = newObj.AddComponent<Button>();
+        ColorBlock colorVar = newButton.colors;
+        colorVar.highlightedColor = new Color(91, 192, 41);
+        newButton.colors = colorVar;
+        return newButton;
     }
 
-    private float calculateImageXPosition(float xPosition){
-        return xPosition * 100; 
+    private void setPositionOfItemInInventory(GameObject newObj) {
+        newObj.GetComponent<RectTransform>().SetParent(parentPanel.transform);
+        Vector3 topRight = getTopRightCorner();
+        newObj.transform.localPosition = generatePositionWithOffset(topRight);
     }
 
-    private float calculateImageYPosition(float yPosition){
-        return yPosition * 100; 
+    private static void enableInventoryItem(GameObject newObj) {
+        newObj.SetActive(true);
     }
 
-    private void generateNextPosition(){
+    private Vector3 generatePositionWithOffset(Vector3 originalPosition) {
+        incrementInventoryTrackers();
+        Vector3 nextInventoryPosition = generateCurrentInventoryPosition(originalPosition);
+        return nextInventoryPosition;
+    }
+
+    private Vector3 generateCurrentInventoryPosition(Vector3 originalPosition) {
+        float x = originalPosition.x + xStartingPosition * 100 + 50;
+        float y = originalPosition.y + yStartingPosition * 100 + 50;
+        float z = 0;
+        return new Vector3(x, y, z);
+    }
+
+    private void incrementInventoryTrackers(){
         if (yStartingPosition == 9){
             Debug.Log("Last Y Position");    
         }
@@ -190,19 +118,41 @@ public class InventoryUIHandler : MonoBehaviour
     
     }
 
+    private void initializeInventoryTrackerValues() {
+        inventoryHeight = 600;
+        inventoryWidth = 700;
 
-    private Vector3 generatePositionWizthOffset(Vector3 originalPosition, float xOffset, float yOffset, float zOffset){
-        generateNextPosition();
-        float x = originalPosition.x + xStartingPosition * 100 + 50;
-        float y = originalPosition.y + yStartingPosition * 100 + 50;
-        float z = 0;
-        return new Vector3(x,y,z);
+        maxInventoryItemsX = 10;
+        maxInventoryItemsY = 10;
+
+        inventorySizeXAxis = 10;
+
+        inventoryPopulatedYPosition = 0;
+        inventoryPopulatedXPosition = 0;
+
+        xStartingPosition = -1;
+        yStartingPosition = 0;
+    }
+    private void populateInventoryColumns() {
+        inventoryMatrix = new List<List<Image>>();
+        for (int columnsOfInventory = 0; columnsOfInventory < inventorySizeXAxis; columnsOfInventory++) {
+            List<Image> row = new List<Image>();
+            inventoryMatrix.Add(row);
+        }
     }
 
-    private void toggleInventoryPanel(){
-        if (Input.GetKeyDown (KeyCode.Tab)){
-            ParentPanel.gameObject.SetActive(!ParentPanel.gameObject.active);
+    private void toggleInventoryPanel() {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            parentPanel.gameObject.SetActive(!parentPanel.gameObject.active);
         }
+    }
+
+    public Vector3 getTopRightCorner() {
+        Vector3[] v = new Vector3[4];
+        parentPanel.GetLocalCorners(v);
+        return v[0];
+
     }
 
 }
